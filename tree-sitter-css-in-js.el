0942f650327afeecf5d5b5a7820408026b9594a0
Dan Orzechowski <orzechod@gmail.com>
2022-08-07 21:38:21 -0400

    add tarball var

---
+++ tree-sitter-css-in-js.el
@@ -0,0 +1,12 @@
+;;; tree-sitter-css-in-js.el --- elisp bindings -*- lexical-binding: t; -*-
+;;; Version: 0.0.1
+;;; Summary:
+;;; Commentary:
+;;; Code:
+
+
+(defvar tree-sitter-css-in-js--archive
+  "https://github.com/orzechowskid/tree-sitter-css-in-js/releases/download/latest/css_in_js.so.tar.gz"
+  "Location of tarball containing tree-sitter CSS-in-JS binaries.")
+
+(provide 'tree-sitter-css-in-js)
